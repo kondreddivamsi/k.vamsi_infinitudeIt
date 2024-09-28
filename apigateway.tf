33c59745b64114017cb16655c70bc63c348a419f
kondreddivamsi <vamsiram1227@gmail.com>
2024-09-28 14:18:31 +0530

    apigateway.tf

---
+++ apigateway.tf
@@ -0,0 +1,27 @@
+resource "aws_api_gateway_rest_api" "my_api" {
+  name        = "MyAPI"
+  description = "My API Gateway"
+}
+
+resource "aws_api_gateway_resource" "my_resource" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  parent_id   = aws_api_gateway_rest_api.my_api.root_resource_id
+  path_part   = "hello"
+}
+
+resource "aws_api_gateway_method" "get_method" {
+  rest_api_id   = aws_api_gateway_rest_api.my_api.id
+  resource_id   = aws_api_gateway_resource.my_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "get_integration" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.my_resource.id
+  http_method = aws_api_gateway_method.get_method.http_method
+
+  integration_http_method = "GET"
+  type                    = "HTTP"
+  uri                     ="http://54.236.245.128/hello"  # Replace with your EC2 instance IP or a Lambda function URL
+}
