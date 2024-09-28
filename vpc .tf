17f4cfcf080c16e5e1cebebd5132cb147125b14f
kondreddivamsi <vamsiram1227@gmail.com>
2024-09-28 14:23:30 +0530

    Create vpc .tf

---
+++ vpc .tf
@@ -0,0 +1,19 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "main-subnet"
+  }
+}
